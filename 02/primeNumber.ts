7bd38174558d38b8869539fdafde8f6c3d5672b6
0tuedon <tuedonmofe@gmail.com>
2022-05-07 11:33:07 +0100

    Week-2 algo prime numbers

---
+++ 02/primeNumber.ts
@@ -0,0 +1,19 @@
+// Algo to get Prime Number or Not
+// Note 1 is not a prime Number. Prime Number is a number that is only divisible by itself
+// AND NO OTHER NUMBER 
+
+const isPrime = (x:number):boolean=>{
+    let divisibleNumber = []
+    for(let i = 1; i < x; i++){
+        if(x % i === 0){
+            divisibleNumber.push(i);
+        }
+        else{
+            
+        }
+    }
+    return x!==1 && divisibleNumber.length <= 1 
+}
+
+
+console.log(isPrime(14))
